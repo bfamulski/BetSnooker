d48c920a136fba9990398ccef0fa76db53e3b6d7
Boguslaw Famulski <boguslaw.famulski@gmail.com>
2023-05-16 20:38:29 +0200

    add tailwind css

---
+++ react-web-app/src/style/typedTailwind/index.ts
@@ -0,0 +1,4 @@
+export { default as twCls } from './tailwindcss.generated';
+export * from './tailwindcss.generated';
+export { default as combineCls } from 'classnames';
+export { customTailwindCssClassnames } from './tailwindcss.customClasses';
---
+++ react-web-app/src/style/typedTailwind/tailwindcss.customClasses.ts
@@ -0,0 +1,5 @@
+type Bodies = 'body';
+type CustomTailwindCssClassnames = Bodies;
+
+export const customTailwindCssClassnames = {} as CustomTailwindCssClassnames;
+export default CustomTailwindCssClassnames;
